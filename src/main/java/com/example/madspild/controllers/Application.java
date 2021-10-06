b67f79eacb6eab5208beca29f9663e65d4e21107
Michala Nybroe <michalanim@gmail.com>
2021-10-06 19:48:15 +0200

    add thymeleaf, controller and changes in reference to thymeleaf

--- src/main/java/com/example/madspild/controllers/Application.java
+++ src/main/java/com/example/madspild/controllers/Application.java
@@ -10,4 +10,29 @@ public class Application {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/om")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/ideer")
+    public String ideas() {
+        return "ideas";
+    }
+
+    @GetMapping("/donation")
+    public String donate() {
+        return "donate";
+    }
+
+    @GetMapping("/kontakt")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/subscribe")
+    public String subscribe() {
+        return "subscribe";
+    }
 }
