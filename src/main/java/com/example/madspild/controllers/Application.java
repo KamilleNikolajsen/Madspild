f25d8c4b162b7a0e66e455f6c422a4bb15c23059
Rasmus Kibshede <2740ralle@gmail.com>
2021-10-07 09:26:52 +0200

    removed method frontpage

--- src/main/java/com/example/madspild/controllers/Application.java
+++ src/main/java/com/example/madspild/controllers/Application.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Application {
 
-    @GetMapping("/forside")
-    public String frontpage() {
-        return "index";
-    }
-
     @GetMapping("/")
     public String index() {
-        return index();
+        return "index";
     }
 
     @GetMapping("/om")
